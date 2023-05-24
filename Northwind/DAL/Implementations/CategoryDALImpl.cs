165060956c996b8736828adc28fedcb7eefa35bb
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-05-23 19:01:16 -0600

    Generic y GetAllCategeories DAL

---
+++ Northwind/DAL/Implementations/CategoryDALImpl.cs
@@ -0,0 +1,71 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class CategoryDALImpl : ICategoryDAL
+    {
+        private NorthWindContext _northWindContext;
+        private UnidadDeTrabajo<Category> unidad;
+
+        public bool Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Category> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> Find(Expression<Func<Category, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            IEnumerable<Category> categories = null;
+            using (unidad = new UnidadDeTrabajo<Category>(new NorthWindContext()))
+            {
+                categories = unidad.genericDAL.GetAll();
+
+
+            }
+
+            return categories;
+
+        }
+
+        public bool Remove(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Category> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category SingleOrDefault(Expression<Func<Category, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
