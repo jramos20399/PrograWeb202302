9de067032bf2d313bc2d73270ec303502654a899
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-05-30 19:04:11 -0600

    CategoryHelper GetALL

---
+++ Northwind/FrontEnd/Helpers/CategoryHelper.cs
@@ -0,0 +1,38 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class CategoryHelper
+    {
+        ServiceRepository repository;
+
+        public CategoryHelper()
+        {
+             repository = new ServiceRepository();
+        }
+
+
+        public List<CategoryViewModel> GetAll()
+        {
+
+            
+
+            List<CategoryViewModel> lista = new List<CategoryViewModel>();
+            HttpResponseMessage responseMessage = repository.GetResponse("api/Category/");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<CategoryViewModel>>(content);
+
+            }
+
+
+
+
+            return lista;
+        }
+
+
+    }
+}
