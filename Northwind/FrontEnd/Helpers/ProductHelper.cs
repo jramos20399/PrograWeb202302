32ea5bc170cd675b599be698e4c2a23abba4889d
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-07-04 18:55:37 -0600

    Supplier y Product Helper terminados

---
+++ Northwind/FrontEnd/Helpers/ProductHelper.cs
@@ -0,0 +1,68 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class ProductHelper
+    {
+        private ServiceRepository ServiceRepository;
+
+
+        public ProductHelper()
+        {
+            ServiceRepository = new ServiceRepository();
+
+        }
+
+
+
+        public List<ProductViewModel> GetAll()
+        {
+            List<ProductViewModel> lista = new List<ProductViewModel>();
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/product");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<ProductViewModel>>(content);
+            }
+
+            return lista;
+
+        }
+
+
+
+
+        public ProductViewModel Get(int id)
+        {
+            ProductViewModel ProductViewModel;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/product/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            ProductViewModel = JsonConvert.DeserializeObject<ProductViewModel>(content);
+
+
+
+            return ProductViewModel;
+        }
+
+
+        public ProductViewModel Create(ProductViewModel product)
+        {
+
+
+            ProductViewModel ProductViewModel;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.PostResponse("api/product/", product);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            ProductViewModel = JsonConvert.DeserializeObject<ProductViewModel>(content);
+
+
+
+            return ProductViewModel;
+        }
+    }
+}
---
+++ Northwind/FrontEnd/Helpers/SupplierHelper.cs
@@ -0,0 +1,54 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class SupplierHelper
+    {
+
+
+        private ServiceRepository ServiceRepository;
+
+
+        public SupplierHelper()
+        {
+            ServiceRepository = new ServiceRepository();
+
+        }
+
+
+        public List<SupplierViewModel> GetAll()
+        {
+
+            List<SupplierViewModel> lista = new List<SupplierViewModel>();
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/supplier");
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                lista = JsonConvert.DeserializeObject<List<SupplierViewModel>>(content);
+            }
+
+            return lista;
+
+
+        }
+
+
+        public SupplierViewModel Get(int id)
+        {
+            SupplierViewModel supplier;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/supplier/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            supplier = JsonConvert.DeserializeObject<SupplierViewModel>(content);
+
+
+
+            return supplier;
+        }
+
+    }
+}
