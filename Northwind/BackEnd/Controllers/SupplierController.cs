6bd47665992d2a280bc3140c7d28f1b621388624
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-07-04 18:52:12 -0600

    Supplier y Product controller api terminados

---
+++ Northwind/BackEnd/Controllers/SupplierController.cs
@@ -0,0 +1,56 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierController : ControllerBase
+    {
+        private ISupplierDAL supplierDAL;
+
+        public SupplierController()
+        {
+            supplierDAL = new SupplierDALImpl();
+        }
+
+        // GET: api/<SupplierController>
+        [HttpGet]
+        public async Task<JsonResult> Get()
+        {
+            IEnumerable<Supplier> suppliers= await supplierDAL.GetAll();
+
+            
+            return new JsonResult(suppliers);
+        }
+
+        // GET api/<SupplierController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SupplierController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SupplierController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SupplierController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
