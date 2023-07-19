482be1ce47ffec4f28ef53957517365565e159d0
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-07-18 18:31:03 -0600

    Connection in appsetings.json

--- Northwind/BackEnd/Program.cs
+++ Northwind/BackEnd/Program.cs
@@ -1,4 +1,25 @@
+using Entities.Entities;
+using Entities.Utilities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+#region Connection String
+builder.Services.AddDbContext<NorthWindContext>(options =>
+                        options.UseSqlServer(
+                            builder
+                            .Configuration
+                            .GetConnectionString("DefaultConnection")));
+
+string connString = builder
+                            .Configuration
+                            .GetConnectionString("DefaultConnection");
+
+Util.ConnectionString = connString;
+
+#endregion
+
 
 // Add services to the container.
 
