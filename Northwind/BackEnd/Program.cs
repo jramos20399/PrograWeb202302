432b9c0640e0ae10db33e5230c97c907ae74aa6a
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-07-25 18:46:18 -0600

    JWT appssettings

--- Northwind/BackEnd/Program.cs
+++ Northwind/BackEnd/Program.cs
@@ -2,8 +2,11 @@ using BackEnd.Middleware;
 using Entities.Authentication;
 using Entities.Entities;
 using Entities.Utilities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,6 +49,33 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 
 
 #region JWT
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
+
+
+
+
 #endregion
 
 // Add services to the container.
